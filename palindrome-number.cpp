875614a7324200631bc1566f1cfbc9d407789b66
Manan Sukhani <manamsukhani@gmail.com>
2023-02-08 18:40:31 +0530

    Add files via upload

---
+++ palindrome-number.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int orig, rev=0, rem;
+        orig = x;
+        while(x>0){
+            rem = x%10;
+            rev = (rev*10)+rem;
+            x = x/10;
+        }
+        if(orig == rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
